ebbb33f5590ec9a3f8d6d25f013e1361d06d15f6
Preston Button <prestonbutton99@gmail.com>
2023-09-07 23:00:39 -0400

    Almost there. One problem is that the last input is not being dealt with properly. On running the program I have to hit enter twice for it to display the last Bingo or no Bingo.

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
@@ -9,76 +8,68 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Bingo Verifier");
 
-        // 2D array to represent the pattern
-        int[][] pattern = new int[5][5];
-
-        // List to store the called numbers
-        List<Integer> calledNumbers = new ArrayList<>();
-
         // Create a Scanner object for reading the input
         Scanner scanner = new Scanner(System.in);
 
         try {
-            // Read the pattern
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    pattern[i][j] = scanner.nextInt();
+            while (true) {
+                // 2D array to represent the pattern
+                int[][] pattern = new int[5][5];
+
+                // List to store the called numbers
+                List<Integer> calledNumbers = new ArrayList<>();
+
+                // Read the pattern
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        pattern[i][j] = scanner.nextInt();
+                    }
+                    scanner.nextLine(); // Consume the newline character at the end of each row
                 }
-            }
 
-            // Print the pattern
-            System.out.println("Pattern:");
-            for (int[] row : pattern) {
-                System.out.println(Arrays.toString(row));
-            }
+                // Skip the blank line
+                scanner.nextLine();
 
-            // Skip the blank line
-            scanner.nextLine();            
-            scanner.nextLine();
-
-
-            // Read the called numbers
-            String[] numbers = scanner.nextLine().split(" ");
-            for (String number : numbers) {
-                int num = Integer.parseInt(number);
-                calledNumbers.add(num);
-            }
-
-            // Print the called numbers
-            System.out.println("Called numbers: " + calledNumbers);
-
-            // Convert calledNumbers to a Set for faster lookups
-            Set<Integer> calledNumbersSet = new HashSet<>(calledNumbers);
-
-            // Skip the blank line
-            scanner.nextLine();
-
-            // 2D array to represent the bingo card
-            int[][] bingoCardNumbers = new int[5][5];
-
-            // Read the bingo card
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    bingoCardNumbers[i][j] = scanner.nextInt();
+                // Read the called numbers
+                String[] numbers = scanner.nextLine().split(" ");
+                for (String number : numbers) {
+                    int num = Integer.parseInt(number);
+                    calledNumbers.add(num);
                 }
-            }
 
-            // Print the bingo card
-            System.out.println("Bingo Card:");
-            for (int[] row : bingoCardNumbers) {
-                System.out.println(Arrays.toString(row));
-            }
+                // Convert calledNumbers to a Set for faster lookups
+                Set<Integer> calledNumbersSet = new HashSet<>(calledNumbers);
 
-            BingoCard card = new BingoCard(bingoCardNumbers);
+                // Skip the blank line
+                scanner.nextLine();
 
-            // Create a BingoVerifier object
-            BingoVerifier verifier = new BingoVerifier(pattern, calledNumbersSet, card);
+                // 2D array to represent the bingo card
+                int[][] bingoCardNumbers = new int[5][5];
 
-            // Print the result
-            if (verifier.isValidBingo()) {
-                System.out.println("VALID BINGO");
-            } else {
-                System.out.println("NO BINGO");
+                // Read the bingo card
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        bingoCardNumbers[i][j] = scanner.nextInt();
+                    }
+                    scanner.nextLine(); // Consume the newline character at the end of each row
+                }
+
+                BingoCard card = new BingoCard(bingoCardNumbers);
+
+                // Create a BingoVerifier object
+                BingoVerifier verifier = new BingoVerifier(pattern, calledNumbersSet, card);
+
+                // Print the result
+                if (verifier.isValidBingo()) {
+                    System.out.println("VALID BINGO");
+                } else {
+                    System.out.println("NO BINGO");
+                }
+
+                // Check if there's more input
+                if (!scanner.hasNext()) {
+                    break;
+                }
             }
         } finally {
             scanner.close();
